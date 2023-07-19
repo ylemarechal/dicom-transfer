a625bfb0a3330ecb6f3820af3b87863daa501a49
Gabriel Couture <gacou54@gmail.com>
2023-07-19 14:40:52 -0400

    Allowing the use of a csv file for accession numbers (#2)
    Feat: 
    * Allowing the use of a csv file for accession numbers
    * Adding argument to specify header or without header for the csv file

--- exams/__init__.py
+++ exams/__init__.py
@@ -1,4 +1,30 @@
-ACCESSION_NUMBERS=[
-  "PATIENT_ACC_NUM1",
-  "PATIENT_ACC_NUM2"
+import sys
+
+ACCESSION_NUMBERS = [
+    "PATIENT_ACC_NUM1",
+    "PATIENT_ACC_NUM2"
 ]
+
+
+def get_accession_numbers():
+    number_of_args = len(sys.argv)
+
+    if number_of_args == 1:  # Correspond to the executed python file
+        return ACCESSION_NUMBERS
+
+    elif number_of_args == 2:
+        with open(sys.argv[1]) as file:
+            lines = file.readlines()
+
+            return [line.replace('\n', '') for line in lines]
+
+    elif number_of_args == 3:
+        with open(sys.argv[1]) as file:
+            lines = file.readlines()
+
+            if 'with_header' in sys.argv:
+                return [line.replace('\n', '') for line in lines[1:]]
+            elif 'without_header' in sys.argv:
+                return [line.replace('\n', '') for line in lines]
+
+    raise ValueError('Too many arguments')
